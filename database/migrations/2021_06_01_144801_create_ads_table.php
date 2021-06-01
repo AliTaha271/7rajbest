8a79df90540451ed0463988f58d61eb3221e5f62
Ali-Taha271 <73026810+Ali-Taha271@users.noreply.github.com>
2021-06-01 21:26:06 +0300

    Auth login + Database tables

---
+++ database/migrations/2021_06_01_144801_create_ads_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAdsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ads', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id')->unsigned();
+            $table->bigInteger('city_id')->unsigned();
+            $table->bigInteger('country_id')->unsigned();
+            $table->bigInteger('category_id')->unsigned();
+            $table->string('title');
+            $table->mediumText('discrebtion');
+            $table->integer('price');
+            $table->integer('7raj_price');
+            $table->string('image1');
+            $table->string('image2')->nullable();
+            $table->string('image3')->nullable();
+            $table->integer('is_new')->nullable();
+            $table->integer('note_why')->nullable();
+            $table->string('phone');
+            $table->string('stute')->default('waiting');
+            $table->string('wow');
+            // yes is " wow "
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ads');
+    }
+}
